e931d49b9dbf66803c3b95a016ee985123cd7a12
pavlushaemae <pavel.pidikseev173@gmail.com>
2022-07-05 16:02:26 +0300

    Added navigation

--- app/src/main/java/com/itis/devinterview/ui/activity/MainActivity.kt
+++ app/src/main/java/com/itis/devinterview/ui/activity/MainActivity.kt
@@ -2,11 +2,17 @@ package com.itis.devinterview.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.itis.devinterview.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var controller: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        controller =
+            (supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment)
+                .navController
     }
 }
