bb3b7ba835db546601647af6f4f01ef4219419ed
pavlushaemae <pavel.pidikseev173@gmail.com>
2022-07-05 15:43:17 +0300

    Added packages

--- app/src/main/java/com/itis/devinterview/ui/activity/MainActivity.kt
+++ app/src/main/java/com/itis/devinterview/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.itis.devinterview
+package com.itis.devinterview.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.itis.devinterview.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
