2008e1380e07327072f26fbe3eb7e3433974bac0
GorlovDanila <danildanil20030@gmail.com>
2022-07-06 14:12:16 +0300

    30%

--- app/src/main/java/com/itis/devinterview/ui/activity/MainActivity.kt
+++ app/src/main/java/com/itis/devinterview/ui/activity/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.itis.devinterview.R
-import com.itis.devinterview.preferences.SharedPrefRepository
 
 class MainActivity : AppCompatActivity() {
     private lateinit var controller: NavController
