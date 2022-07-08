51d80260af6f0c76c445dd369cb53753d3e0960f
w1nway <90196953+w1nway@users.noreply.github.com>
2022-07-08 19:06:37 +0300

    "exam with broken timer"

--- app/src/main/java/com/itis/devinterview/ui/fragment/ExamFragment.kt
+++ app/src/main/java/com/itis/devinterview/ui/fragment/ExamFragment.kt
@@ -1,29 +1,27 @@
 package com.itis.devinterview.ui.fragment
 
 import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import com.google.android.material.tabs.TabLayoutMediator
 import com.itis.devinterview.R
 import com.itis.devinterview.databinding.FragmentExamBinding
-import com.itis.devinterview.model.Question
 import com.itis.devinterview.preferences.AccessToRepository
 import com.itis.devinterview.service.impl.QuestionServiceImpl
-import com.itis.devinterview.ui.activity.ExamAdapter
 
 open class ExamFragment : Fragment(R.layout.fragment_exam) {
 
+    private var timer: CountDownTimer? = null
     private var _binding: FragmentExamBinding? = null
     private val binding get() = _binding!!
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentExamBinding.bind(view)
+
+        startCountDownTimer(5*60*1000)
 
         val pref = activity?.getSharedPreferences("TEST", Context.MODE_PRIVATE)
         if (pref != null) {
@@ -44,6 +42,19 @@ open class ExamFragment : Fragment(R.layout.fragment_exam) {
             btnThirdAns.text = textQuestion?.third
             btnFourthAns.text = textQuestion?.fourth
         }
+    }
+
+    private fun startCountDownTimer(timeMillis: Long) {
+        timer?.cancel()
+        timer = object : CountDownTimer(timeMillis, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                binding.tvTimer.text = millisUntilFinished.toString()
+            }
+
+            override fun onFinish() {
+                binding.tvTimer.text = "Finish"
+            }
+        }.start()
     }
 
     override fun onDestroyView() {
