462218f784e16432f008a6a625d899e75e299271
Динара <zaripovadin@yandex.ru>
2022-07-10 14:16:43 +0300

    new education

--- app/src/main/java/com/itis/devinterview/ui/fragment/KotlinFragment.kt
+++ app/src/main/java/com/itis/devinterview/ui/fragment/KotlinFragment.kt
@@ -3,5 +3,5 @@ package com.itis.devinterview.ui.fragment
 import androidx.fragment.app.Fragment
 import com.itis.devinterview.R
 
-class KotlinFragment: Fragment(R.layout.kotlin_fragment) {
+class KotlinFragment: Fragment(R.layout.fragment_kotlin) {
 }
