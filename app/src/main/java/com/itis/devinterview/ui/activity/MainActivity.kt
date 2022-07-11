209dbff7f66bbd3b2b02089c94c90e8ab6a1136f
pavlushaemae <pavel.pidikseev173@gmail.com>
2022-07-12 02:45:46 +0300

    corrected ALL

--- app/src/main/java/com/itis/devinterview/ui/activity/MainActivity.kt
+++ app/src/main/java/com/itis/devinterview/ui/activity/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.itis.devinterview.R
+import com.itis.devinterview.preferences.AccessToRepository
 
 class MainActivity: AppCompatActivity() {
     private lateinit var controller: NavController
@@ -14,7 +15,7 @@ class MainActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         controller = (supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment).navController
-
+        AccessToRepository.addLightThemeEnabled()
         val bottomView = findViewById<BottomNavigationView>(R.id.bottom_view)
         bottomView.setupWithNavController(controller)
     }
--- app/src/main/java/com/itis/devinterview/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/com/itis/devinterview/ui/fragment/SettingsFragment.kt
@@ -6,22 +6,26 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.itis.devinterview.databinding.FragmentSettingsBinding
+import com.itis.devinterview.preferences.AccessToRepository
 
 class SettingsFragment : Fragment(com.itis.devinterview.R.layout.fragment_settings) {
 
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentSettingsBinding.bind(view)
-
         with(binding) {
             switchThemes.setOnCheckedChangeListener { _, checkedId ->
                 when (checkedId) {
-                    true -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-
-                    false -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    true -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                        AccessToRepository.addNightThemeEnabled()
+                    }
+                    false -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                        AccessToRepository.addLightThemeEnabled()
+                    }
                 }
             }
             tvResetStatistics.setOnClickListener {
